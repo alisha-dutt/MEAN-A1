68772f688fe05dafea2f5c076d5edc52ba68bb5f
alisha-dutt <200490302@student.georgianc.on.ca>
2023-02-10 14:04:48 -0500

    added some content to the views

--- controllers/index.js
+++ controllers/index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/',(req, res, next)=> {
+router.get('/',(req, res)=> {
   res.render('index', { title: 'Home' });
 });
 
 /* GET about page. */
-router.get('/about',(req, res, next)=> {
+router.get('/about',(req, res)=> {
   res.render('about', { title: 'About Me' });
 });
 
 /* GET goals page. */
-router.get('/goals',(req, res, next)=> {
+router.get('/goals',(req, res)=> {
   res.render('goals', { title: 'Goals & Intrests' });
 });
 
--- controllers/members.js
+++ controllers/members.js
@@ -3,10 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/',(req, res, next)=> {
-  res.render('index', { title: 'Family Members' });
+  res.render('members/index', { title: 'Family Members' });
 });
 /* GET /father */
-router.get('/father',(req, res)=> {
+router.get('members/father',(req, res)=> {
   res.render('father', { title: 'Father' });
 });
 
