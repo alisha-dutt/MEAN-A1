3f2f575f263899d871a80876753a74c340a7731e
Naruna07 <200483262@student.georgianc.on.ca>
2023-05-19 21:35:40 -0400

    fixed bugs

--- controllers/friends.js
+++ controllers/friends.js
@@ -5,10 +5,7 @@ const router = express.Router();
 router.get('/',(req, res, next)=> {
   res.render('friends/index', { title: 'Friends' });
 });
-/* GET /bestfriend1 */
-router.get('/bestfriendmale',(req, res)=> {
-  res.render('friends/bestfriendmale', { title: 'My Best friend' });
-});
+
 
 /* GET /bestfriend2 */
 router.get('/bestfriendfemale',(req, res)=> {
